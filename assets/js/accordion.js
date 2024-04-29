8428d5a949f67163b4cc6df1c0c71c375d5fb242
hannes hoffmann <hannes.hoffmann@student.tugraz.at>
2024-04-29 10:19:48 +0200

    no fix

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -5,25 +5,25 @@ for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    var isOpen = panel.style.maxHeight;
+    
     for(j = 0; j < acc.length; j++){
         var otherpanel = acc[j].nextElementSibling;
-        if (acc[j] !== this)
+        if (otherpanel !== panel)
         {
             acc[j].classList.remove("active");
-            if (otherpanel.style.maxHeight) 
-            {
-                otherpanel.style.maxHeight = 0;
-            } 
+            otherpanel.style.maxHeight = null;
         }
     }
-    this.classList.toggle("active");
-
+    
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = 0;
+
+    if (isOpen) {
+      panel.style.maxHeight = null;
     } else {
-      panel.style.maxHeight = 500;
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 } 
