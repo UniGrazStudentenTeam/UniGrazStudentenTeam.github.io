efff32a120d597cfc523428efbe3300a8432bf5b
hannes hoffmann <hannes.hoffmann@student.tugraz.at>
2024-04-29 09:50:52 +0200

    less bug

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -6,10 +6,14 @@ for (i = 0; i < acc.length; i++) {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     for(i = 0; i < acc.length; i++){
-       acc[i].classList.toggle("active");
-       var panel = this.nextElementSibling;
-       panel.style.maxHeight = null; 
-       acc[i].classList.remove("active");
+   
+       
+        acc[i].classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } 
+        acc[i].classList.remove("active");
     }
     this.classList.toggle("active");
 
